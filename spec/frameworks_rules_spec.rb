2b0071e8b0e091bc8368a9b05dddfec4400e5b9e
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2015-10-22 10:30:03 +0200

    explode additional rule specs for all gem based rules
    We want full spec coverage for our matchers. That currently means we
    duplicate the added specs for gem based rules from Rails to the other
    ones.
    
    We might introduce some extended rule matchers for such cases which
    would allow to drop the number of copied specs later on.

--- spec/frameworks_rules_spec.rb
+++ spec/frameworks_rules_spec.rb
@@ -26,7 +26,7 @@ describe 'Frameworks rules' do
       expect(repository.primary_frameworks).to eq ['Rails']
     end
 
-    it 'does not returns Rails when commented out' do
+    it 'does not return Rails when commented out' do
       repository = repository('Gemfile', "bogus\n# gem 'rails'\nbogus")
       expect(repository.primary_frameworks).to_not eq ['Rails']
     end
@@ -52,6 +52,21 @@ describe 'Frameworks rules' do
       expect(repository.primary_frameworks).to eq ['Sinatra']
       expect(repository.secondary_frameworks).to eq []
     end
+
+    it 'returns Sinatra when specified with version' do
+      repository = repository('Gemfile', "bogus\ngem 'sinatra', '~> 4.3'\nbogus")
+      expect(repository.primary_frameworks).to eq ['Sinatra']
+    end
+
+    it 'returns Sinatra when indented' do
+      repository = repository('Gemfile', "bogus\n  gem 'sinatra'\nbogus")
+      expect(repository.primary_frameworks).to eq ['Sinatra']
+    end
+
+    it 'does not return Sinatra when commented out' do
+      repository = repository('Gemfile', "bogus\n# gem 'sinatra'\nbogus")
+      expect(repository.primary_frameworks).to_not eq ['Sinatra']
+    end
   end
 
   describe 'Dashing' do
@@ -60,6 +75,21 @@ describe 'Frameworks rules' do
       expect(repository.primary_frameworks).to eq ['Sinatra']
       expect(repository.secondary_frameworks).to eq ['Dashing']
     end
+
+    it 'returns Sinatra when specified with version' do
+      repository = repository('Gemfile', "bogus\ngem 'dashing', '~> 4.3'\nbogus")
+      expect(repository.primary_frameworks).to eq ['Sinatra']
+    end
+
+    it 'returns Sinatra when indented' do
+      repository = repository('Gemfile', "bogus\n  gem 'dashing'\nbogus")
+      expect(repository.primary_frameworks).to eq ['Sinatra']
+    end
+
+    it 'does not return Sinatra when commented out' do
+      repository = repository('Gemfile', "bogus\n# gem 'dashing'\nbogus")
+      expect(repository.primary_frameworks).to_not eq ['Sinatra']
+    end
   end
 
   describe 'Middleman' do
@@ -67,6 +97,21 @@ describe 'Frameworks rules' do
       repository = repository('Gemfile', "bogus\ngem 'middleman'\nbogus")
       expect(repository.primary_frameworks).to eq ['Middleman']
       expect(repository.secondary_frameworks).to eq []
+    end
+
+    it 'returns Middleman when specified with version' do
+      repository = repository('Gemfile', "bogus\ngem 'middleman', '~> 4.3'\nbogus")
+      expect(repository.primary_frameworks).to eq ['Middleman']
+    end
+
+    it 'returns Middleman when indented' do
+      repository = repository('Gemfile', "bogus\n  gem 'middleman'\nbogus")
+      expect(repository.primary_frameworks).to eq ['Middleman']
+    end
+
+    it 'does not return Middleman when commented out' do
+      repository = repository('Gemfile', "bogus\n# gem 'middleman'\nbogus")
+      expect(repository.primary_frameworks).to_not eq ['Middleman']
     end
   end
 
