c357a4dd42e229c846a86e379a8af36c143c1330
Alexis Reigel <mail@koffeinfrei.org>
2015-10-25 11:01:56 +0100

    disallow rspec doubles on non-existing method

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,4 +4,11 @@ require 'support/spec_factory'
 
 RSpec.configure do |config|
   config.include SpecFactory
+
+  config.mock_with :rspec do |mocks|
+    # Prevents you from mocking or stubbing a method that does not exist on
+    # a real object. This is generally recommended, and will default to
+    # `true` in RSpec 4.
+    mocks.verify_partial_doubles = true
+  end
 end
