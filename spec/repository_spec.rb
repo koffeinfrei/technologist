1146b3a214afb60bff97f49de8220d7c29865998
Alexis Reigel <mail@koffeinfrei.org>
2015-11-02 14:41:20 +0100

    simplify too integrated spec

--- spec/repository_spec.rb
+++ spec/repository_spec.rb
@@ -1,36 +1,23 @@
 require 'spec_helper'
 
 describe Technologist::Repository do
-  let(:repository) do
-    Technologist::Repository.new('.').tap do |repository|
-      # default file content for non existing files
-      allow(repository.git_repository).to receive(:file_content).and_return(nil)
-
-      # rules
-      allow(repository.framework_detector).to receive(:rules).and_return({
-        'SecondaryFramework' => {
-          'rules' => [
-            FileContentRule.new(file_name: 'file1', file_content_pattern: 'file1_content'),
-          ],
-          'primary' => 'PrimaryFramework'
-        }
-      })
-    end
-  end
-
   describe '#primary_frameworks' do
-    it 'returns PrimaryFramework' do
-      allow(repository.git_repository).to receive(:file_content).with('file1').and_return("file1_content")
+    it 'calls FrameworkDetector#primary_frameworks' do
+      repository = Technologist::Repository.new('.')
 
-      expect(repository.primary_frameworks).to eq ['PrimaryFramework']
+      expect(repository.framework_detector).to receive(:primary_frameworks)
+
+      repository.primary_frameworks
     end
   end
 
   describe '#secondary_frameworks' do
-    it 'returns SecondaryFramework' do
-      allow(repository.git_repository).to receive(:file_content).with('file1').and_return("file1_content")
+    it 'calls FrameworkDetector#secondary_frameworks' do
+      repository = Technologist::Repository.new('.')
 
-      expect(repository.secondary_frameworks).to eq ['SecondaryFramework']
+      expect(repository.framework_detector).to receive(:secondary_frameworks)
+
+      repository.secondary_frameworks
     end
   end
 end
